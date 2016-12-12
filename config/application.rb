d0b5dad92374d99da5d3097750ac94520f72b571
Fernando Blat <ferblape@gmail.com>
2016-12-12 12:41:04 +0100

    Support custom locales

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ Bundler.require(*Rails.groups)
 module GobiertoBudgetsComparator
   class Application < Rails::Application
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'custom', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
     config.i18n.available_locales = [:es, :ca]
 
