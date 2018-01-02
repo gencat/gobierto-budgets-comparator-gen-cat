16d3e77b810a8e6ea7105afeafd9cb590ab91c0f
Fernando Blat <ferblape@gmail.com>
2018-01-02 09:44:01 +0100

    Fix render syntax

--- app/controllers/gobierto_budgets/featured_budget_lines_controller.rb
+++ app/controllers/gobierto_budgets/featured_budget_lines_controller.rb
@@ -19,7 +19,7 @@ module GobiertoBudgets
       if @code.present?
         render pick_template, layout: false
       else
-        render nothing: true, status: 404
+        head :not_found
       end
     end
 
