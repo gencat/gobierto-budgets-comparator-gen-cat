a0243c122430f371d653bcc25c987adb51fa649d
Alberto Miedes <albertomg994@gmail.com>
2018-05-29 16:31:19 +0200

    Fix "see all entities" link

--- app/assets/javascripts/gobierto_budgets/table.js
+++ app/assets/javascripts/gobierto_budgets/table.js
@@ -4,8 +4,13 @@ $(document).on('turbolinks:load', function() {
 
     $(this).closest('table').toggleClass('top-10');
 
-    ($(this).closest('table').hasClass('top-10')) ?
-      $(this).text(I18n.t('gobierto_budgets.places.show.related_entities_budget.see_all', { number: 123 })) :
+    if ($(this).closest('table').hasClass('top-10')) {
+      var totalRows = $("#related-entities-budget-table tbody tr").length;
+      $(this).text(
+        I18n.t('gobierto_budgets.places.show.related_entities_budget.see_all', { number: totalRows })
+      );
+    } else {
       $(this).text(I18n.t('gobierto_budgets.places.show.related_entities_budget.see_less'));
+    }
   })
 });
