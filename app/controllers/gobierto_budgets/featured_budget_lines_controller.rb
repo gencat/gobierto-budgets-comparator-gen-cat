f345caa8674d8259f5c571875ac4d708f5ce9f4b
Alberto Miedes <albertomg994@gmail.com>
2019-03-08 18:02:44 +0100

    Bootstrap featured budget lines embed

--- app/controllers/gobierto_budgets/featured_budget_lines_controller.rb
+++ app/controllers/gobierto_budgets/featured_budget_lines_controller.rb
@@ -1,26 +1,15 @@
 module GobiertoBudgets
   class FeaturedBudgetLinesController < GobiertoBudgets::ApplicationController
 
-    before_action :set_current_organization
+    before_action :set_current_organization, :set_featured_budget_line_attributes
 
     attr_accessor :current_organization
     helper_method :current_organization
 
     def show
       @year = params[:year].to_i
-      @area_name = "functional"
-      @kind = GobiertoBudgets::BudgetLine::EXPENSE
 
-      results = BudgetLine.search(
-        kind: @kind,
-        year: @year,
-        organization_id: current_organization.id,
-        type: @area_name,
-        range_hash: {
-          level: { ge: 3 },
-          amount_per_inhabitant: { gt: 0 }
-        }
-      )["hits"]
+      results = featured_budget_line_candidates(@kind, @year, @area_name)
 
       @code = results.sample["code"] if results.any?
 
@@ -29,12 +18,48 @@ module GobiertoBudgets
       end
     end
 
+    # http://localhost:3000/featured_budget_lines/embed/28079
+    def embed
+      @year = 2018
+
+      @current_organization = Organization.new(slug: "madrid")
+
+      budget_lines = featured_budget_line_candidates(@kind, @year, @area_name)
+
+      @budget_line = budget_lines.sample if budget_lines.any?
+      @code = @budget_line["code"]
+
+      render(action: "embed", layout: "embed")
+    end
+
     private
 
     def set_current_organization
-      @current_organization = Organization.new(slug: params[:id])
+      @current_organization = if params[:organization_slug]
+        Organization.new(slug: params[:organization_slug])
+      elsif params[:organization_id]
+        Organization.new(organization_id: params[:organization_id])
+      end
 
       render_404 and return if @current_organization.nil? || (@current_organization.place.nil? && @current_organization.associated_entity.nil?)
+    end
+
+    def set_featured_budget_line_attributes
+      @area_name = "functional"
+      @kind = GobiertoBudgets::BudgetLine::EXPENSE
+    end
+
+    def featured_budget_line_candidates(kind, year, area_name)
+      BudgetLine.search(
+        kind: kind,
+        year: year,
+        organization_id: @current_organization.id,
+        type: area_name,
+        range_hash: {
+          level: { ge: 3 },
+          amount_per_inhabitant: { gt: 0 }
+        }
+      )["hits"]
     end
 
   end
