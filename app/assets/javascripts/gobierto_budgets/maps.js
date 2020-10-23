2c7c6d7a419e61b50a04db24d953eabc7b2bf9fc
Jorge <jorgeatgu@gmail.com>
2020-10-23 18:49:46 +0200

    refactor to create a newGeoJSONLayer

--- app/assets/javascripts/gobierto_budgets/maps.js
+++ app/assets/javascripts/gobierto_budgets/maps.js
@@ -1,6 +1,8 @@
-$(document).on('turbolinks:load', function () {
+$(document).on('turbolinks:load', function() {
 
   var deckgl
+  var geojsonLayer
+  var CUSTOM_DOMAIN
   new SlimSelect({
     select: '#municipalities-flyTO',
     placeholder: 'Introduce un municipio'
@@ -10,6 +12,19 @@ $(document).on('turbolinks:load', function () {
   var dataTOPOJSON = "https://gist.githubusercontent.com/jorgeatgu/dcb73825b02af45250c4dfa66aa0f94f/raw/18a9f2fa108c56454556abc7e08b64eb2a0dc4d8/municipalities_topojson.json";
   var dataMunicipalities = "https://datos.gobierto.es/api/v1/data/data.csv?sql=SELECT+*+FROM+municipios"
   var endPoint = "https://datos.gobierto.es/api/v1/data/data.csv?sql="
+  var indicator = 'gasto_por_habitante'
+  var year = document.getElementsByTagName('body')[0].getAttribute('data-year')
+  var queryData = `SELECT+${indicator}+,place_id+FROM+indicadores_presupuestos_municipales+WHERE+year=${year}AND+${indicator}+IS+NOT+NULL`;
+
+  var urlData = `${endPoint}${queryData}`
+
+  var indicators = document.querySelectorAll('[data-indicator]')
+
+  indicators.forEach(
+    function(indicator) {
+      indicator.addEventListener("click", loadIndicators);
+    }
+  );
 
   var CHOROPLET_SCALE = [
     [255, 255, 201],
@@ -21,7 +36,6 @@ $(document).on('turbolinks:load', function () {
     [14, 39, 118]
   ]
 
-
   var INITIAL_VIEW_STATE = {
     latitude: 40.416775,
     longitude: -3.703790,
@@ -30,120 +44,71 @@ $(document).on('turbolinks:load', function () {
     maxZoom: 8
   };
 
+  deckgl = new deck.Deck({
+    canvas: 'map',
+    initialViewState: INITIAL_VIEW_STATE,
+    controller: true,
+    getTooltip,
+    onViewStateChange: ({ viewState }) => deckgl.setProps({ viewState })
+  });
 
-  var indicatorValue = 'gasto_por_habitante'
-  var year = document.getElementsByTagName('body')[0].getAttribute('data-year')
-  var queryData = `SELECT+${indicatorValue}+,place_id+FROM+indicadores_presupuestos_municipales+WHERE+year=${year}AND+${indicatorValue}+IS+NOT+NULL`;
-
-  function getValuesIndicators() {
-    var populationAndCostQuery = `SELECT+SUM%28population%29+AS+population%2C+SUM%28gasto_total%29+AS+gasto_total+FROM+indicadores_presupuestos_municipales+WHERE+year=${year}`
-    var populationAndCostData = `${endPoint}${populationAndCostQuery}`
-
-    d3.csv(populationAndCostData).then(function (data) {
-      var totalCost = +data[0].gasto_total
-      var totalPopulation = +data[0].population
-      var costPerHabitant = (totalCost / totalPopulation)
-    })
-
-    var debtQuery = `SELECT+sum%28debt%29+AS+debt+FROM+indicadores_presupuestos_municipales+WHERE+year=${year}`
-    var debtData = `${endPoint}${debtQuery}`
-
-    d3.csv(debtData).then(function (data) {
-      var totalDebt = +data[0].debt
-    })
-  }
-
-  var urlData = `${endPoint}${queryData}`
-
-  var indicators = document.querySelectorAll('[data-indicator]')
-
-  indicators.forEach(
-    function(indicator) {
-     indicator.addEventListener("click", loadIndicators);
+  function getTooltip(_ref) {
+    var object = _ref.object;
+    if (object) {
+      return {
+        html: "<h3 class=\"tooltip-name\">".concat(object.properties.name, "</h3>\n <span style=\"tooltip-value\">Presupuesto: <b style=\"font-size: .65rem;\">").concat(object[indicator], "\u20AC<b></span>"),
+        style: {
+          backgroundColor: '#FFF',
+          fontFamily: 'BlinkMacSystemFont, -apple-system',
+          fontSize: '.65rem',
+          borderRadius: '2px',
+          padding: '0.5rem',
+          boxShadow: '2px 2px 2px 1px rgba(0,0,0,0.1)'
+        }
+      };
     }
-  );
-
-  function loadIndicators(e) {
-    var year = document.getElementsByTagName('body')[0].getAttribute('data-year')
-    var indicatorValue = e.originalTarget.attributes["data-indicator"].nodeValue
-    var queryData = `SELECT+${indicatorValue}+,place_id+FROM+indicadores_presupuestos_municipales+WHERE+year=${year}AND+${indicatorValue}+IS+NOT+NULL`;
-
-    var urlData = `${endPoint}${queryData}`
-
-    initMap(urlData, indicatorValue)
   }
 
-  function initMap(queryData, indicator) {
-    d3.csv(queryData).then(function (data) {
-      data.forEach(function (d) {
+  function redraw() {
+    d3.csv(urlData).then(function(data) {
+      data.forEach(function(d) {
         d.place_id = +d.place_id
         d[indicator] = +d[indicator]
         mapMunicipalities.set(d.place_id, d[indicator]);
       })
 
-      var minValue = d3.min(data, function(d) { return d[indicator]})
-      var maxValue = d3.max(data, function(d) { return d[indicator]})
+      var minValue = d3.min(data, function(d) { return d[indicator] })
+      var maxValue = d3.max(data, function(d) { return d[indicator] })
+      CUSTOM_DOMAIN = [minValue, minValue * 2, minValue * 3, minValue * 4, minValue * 5, minValue * 6, maxValue]
 
       var textMinValue = document.getElementById('map_legend_min_value')
       var textMaxValue = document.getElementById('map_legend_max_value')
       textMinValue.textContent = minValue
       textMaxValue.textContent = maxValue
 
-      d3.json(dataTOPOJSON).then(function (data) {
+      d3.json(dataTOPOJSON).then(function(data) {
 
         var MUNICIPALITIES = topojson.feature(data, data.objects.municipalities);
-        var COLOR_SCALE = d3.scaleThreshold()
-          //FIX: HACK DOMAIN
-          .domain([minValue, minValue*2, minValue*3, minValue*4, minValue*5, minValue*6, minValue*7])
-          .range(CHOROPLET_SCALE);
-
         var geojsonLayer = new deck.GeoJsonLayer({
           id: 'map',
           data: MUNICIPALITIES,
           stroked: false,
           filled: true,
           opacity: 1,
-          getFillColor: function (d) {
-            return COLOR_SCALE(d[indicator] = mapMunicipalities.get(d.properties.cp));
-          },
+          getFillColor,
           pickable: true
         });
 
-        deckgl = new deck.Deck({
-          canvas: 'map',
-          initialViewState: INITIAL_VIEW_STATE,
-          controller: true,
-          layers: [geojsonLayer],
-          getTooltip: function getTooltip(_ref) {
-            var object = _ref.object;
-            if (object && object[indicator]) {
-              return {
-                html: "<h3 class=\"tooltip-name\">".concat(object.properties.name, "</h3>\n <span style=\"tooltip-value\">Presupuesto: <b style=\"font-size: .65rem;\">").concat(object[indicator], "\u20AC<b></span>"),
-                style: {
-                  backgroundColor: '#FFF',
-                  fontFamily: 'BlinkMacSystemFont, -apple-system',
-                  fontSize: '.65rem',
-                  borderRadius: '2px',
-                  padding: '0.5rem',
-                  boxShadow: '2px 2px 2px 1px rgba(0,0,0,0.1)'
-                }
-              };
-            }
-          },
-          updateTriggers: {
-            getFillColor: indicator
-          },
-          onViewStateChange: ({viewState}) => deckgl.setProps({viewState})
-        });
+        deckgl.setProps({layers: [geojsonLayer]});
 
         d3.csv(dataMunicipalities).then(function(data) {
           var nest = d3
             .nest()
-            .key(function(d) { return d.nombre})
+            .key(function(d) { return d.nombre })
             .entries(data);
 
-          nest.sort(function(a, b){
-             return d3.ascending(a.key, b.key);
+          nest.sort(function(a, b) {
+            return d3.ascending(a.key, b.key);
           })
 
           var selectMunicipalities = d3.select('#municipalities-flyTO');
@@ -153,8 +118,8 @@ $(document).on('turbolinks:load', function () {
             .data(nest)
             .enter()
             .append('option')
-            .attr('value', function(d) { return d.key})
-            .text(function(d) { return d.key})
+            .attr('value', function(d) { return d.key })
+            .text(function(d) { return d.key })
 
           var increaseButton = document.getElementById('increaseZoom')
           var decreaseButton = document.getElementById('decreaseZoom')
@@ -180,7 +145,7 @@ $(document).on('turbolinks:load', function () {
           }
 
           function changeStateProps(value, increase) {
-            var increaseDecrease = increase === true ? + 1 : - 1
+            var increaseDecrease = increase === true ? +1 : -1
             deckgl.setProps({
               viewState: {
                 zoom: deckgl.props[value].zoom + increaseDecrease,
@@ -199,7 +164,7 @@ $(document).on('turbolinks:load', function () {
               .property('value')
 
             //Filter municipalities with the selected value
-            var selectElement = data.filter(function (el) {
+            var selectElement = data.filter(function(el) {
               return el.nombre === value
             });
 
@@ -220,7 +185,7 @@ $(document).on('turbolinks:load', function () {
             var strokeDATAFILTER = strokeDATA.features
 
             //Filter by selected municipality
-            var strokeSelected = strokeDATAFILTER.filter(function (el) {
+            var strokeSelected = strokeDATAFILTER.filter(function(el) {
               return el.properties.name === value
             });
 
@@ -235,21 +200,55 @@ $(document).on('turbolinks:load', function () {
               filled: true,
               lineWidthMinPixels: 1,
               opacity: 1,
-              getFillColor: function (d) {
-                return COLOR_SCALE(mapMunicipalities.get(d.properties.cp));
-              },
+              getFillColor,
               pickable: true
             })];
 
             //Update deck.gl with the old and new layer.
-            deckgl.setProps({layers: [geojsonLayer, selectedMunicipality]});
+            deckgl.setProps({ layers: [geojsonLayer, selectedMunicipality] });
           });
         });
       });
     })
-
   }
-  initMap(urlData, indicatorValue)
+
+  function getFillColor(d) {
+    var COLOR_SCALE = d3.scaleThreshold()
+      //FIX: HACK DOMAIN
+      .domain(CUSTOM_DOMAIN)
+      .range(CHOROPLET_SCALE);
+    return COLOR_SCALE(d[indicator] = mapMunicipalities.get(d.properties.cp));
+  }
+
+  function getValuesIndicators() {
+    var populationAndCostQuery = `SELECT+SUM%28population%29+AS+population%2C+SUM%28gasto_total%29+AS+gasto_total+FROM+indicadores_presupuestos_municipales+WHERE+year=${year}`
+    var populationAndCostData = `${endPoint}${populationAndCostQuery}`
+
+    d3.csv(populationAndCostData).then(function(data) {
+      var totalCost = +data[0].gasto_total
+      var totalPopulation = +data[0].population
+      var costPerHabitant = (totalCost / totalPopulation)
+    })
+
+    var debtQuery = `SELECT+sum%28debt%29+AS+debt+FROM+indicadores_presupuestos_municipales+WHERE+year=${year}`
+    var debtData = `${endPoint}${debtQuery}`
+
+    d3.csv(debtData).then(function(data) {
+      var totalDebt = +data[0].debt
+    })
+  }
+
+  function loadIndicators(e) {
+    var year = document.getElementsByTagName('body')[0].getAttribute('data-year')
+    indicator = e.originalTarget.attributes["data-indicator"].nodeValue
+    var queryData = `SELECT+${indicator}+,place_id+FROM+indicadores_presupuestos_municipales+WHERE+year=${year}AND+${indicator}+IS+NOT+NULL`;
+
+    urlData = `${endPoint}${queryData}`
+
+    redraw()
+  }
+
+  redraw()
   getValuesIndicators()
 });
 
