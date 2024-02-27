fe8318bc77e29bcfb42d076721513f7e35ed3237
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-02-27 17:02:25 +0100

    Change cache_path procs

--- app/controllers/gobierto_budgets/api/data_controller.rb
+++ app/controllers/gobierto_budgets/api/data_controller.rb
@@ -24,7 +24,7 @@ module GobiertoBudgets
         :budget_percentage_previous_year,
         :ranking,
         :budget_execution_deviation,
-        cache_path: ->(c) { { locale: I18n.locale, places_collection: places_collection_key } }
+        cache_path: ->(_c) { byebug && { locale: I18n.locale, places_collection: places_collection_key } }
       )
 
       caches_action(
@@ -41,7 +41,7 @@ module GobiertoBudgets
         :budget_percentage_previous_year,
         :ranking,
         :budget_execution_deviation,
-        cache_path: ->(c) { { locale: I18n.locale, places_collection: places_collection_key } }
+        cache_path: -> { { locale: I18n.locale, places_collection: places_collection_key } }
       )
 
       def total_budget
