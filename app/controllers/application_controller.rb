6bedc3c3def9bd8d7936073fa55217be809b3921
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-02-27 12:49:51 +0100

    Split root path into municipios and diputaciones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   rescue_from ActionController::UnknownFormat, with: :render_404
   rescue_from Elasticsearch::Transport::Transport::Errors::BadRequest, with: :render_404
 
-  helper_method :helpers, :users_enabled?, :places_collection_key
+  helper_method :helpers, :users_enabled?, :places_collection_key, :location_root_path
 
   before_action :set_locale, :set_cache_headers
 
@@ -67,7 +67,7 @@ class ApplicationController < ActionController::Base
   end
 
   def check_users_enabled
-    redirect_to root_path and return false unless users_enabled?
+    redirect_to location_root_path and return false unless users_enabled?
   end
 
   def users_enabled?
@@ -79,5 +79,13 @@ class ApplicationController < ActionController::Base
                                  keys = PlaceDecorator.places_keys
                                  keys.include?(params[:places_collection]&.to_sym) ? params[:places_collection].to_sym : keys.first
                                end
+  end
+
+  def location_root_path
+    if places_collection_key == :deputation_eu
+      deputation_root_path
+    else
+      place_root_path
+    end
   end
 end
