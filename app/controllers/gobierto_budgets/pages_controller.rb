2519c583b94843d4a4f9b5d1c236c8d896538024
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-02-29 17:51:39 +0100

    Allow the definition of examples in searches and comparisons from settings

--- app/controllers/gobierto_budgets/pages_controller.rb
+++ app/controllers/gobierto_budgets/pages_controller.rb
@@ -1,7 +1,32 @@
 module GobiertoBudgets
   class PagesController < GobiertoBudgets::ApplicationController
 
+    DEFAULT_SEARCH_EXAMPLES = {
+      ine: [
+        ["Santander", "santander"],
+        ["Talaván", "talavan"],
+        ["Órgiva", "orgiva"],
+        ["Zaragoza", "zaragoza"]
+      ],
+      deputation_eu: [
+        ["Diputación Foral de Vizcaya", "dip-bizkaia"],
+        ["Diputación Foral de Álava", "dip-alava"],
+        ["Diputación Foral de Guipúzcoa", "dip-gipuzkoa"]
+      ]
+    }.freeze
+
+    DEFAULT_COMPARE_EXAMPLES = {
+      ine: [
+        ["Coruña + Pontevedra + Lugo + Ferrol", "coruna-a:pontevedra:lugo:ferrol"],
+        ["Madrid + Barcelona + Valencia + Sevilla", "madrid:barcelona:valencia:sevilla"]
+      ],
+      deputation_eu: [
+        ["Vizcaya + Álava + Guipúzcoa", "dip-bizkaia:dip-alava:dip-gipuzkoa"]
+      ]
+    }.freeze
+
     caches_page :home, :ranking, :deputations_ranking
+    helper_method :search_examples, :compare_examples
 
     def home
     end
@@ -80,6 +105,46 @@ module GobiertoBudgets
           places_collection: @places_collection
         )
       end
+    end
+
+    def search_examples
+      @search_examples ||= begin
+                             return DEFAULT_SEARCH_EXAMPLES if Settings.search_examples.blank?
+
+                             PlaceDecorator.places_keys.each_with_object({}) do |key, examples|
+                               examples[key] = expand_location_values(Settings.search_examples[key], key) || DEFAULT_SEARCH_EXAMPLES[key]
+                             end
+                           end
+    end
+
+    def compare_examples
+      @compare_examples ||= begin
+                              return DEFAULT_COMPARE_EXAMPLES if Settings.compare_examples.blank?
+
+                              PlaceDecorator.places_keys.each_with_object({}) do |key, examples|
+                                examples[key] = if Settings.compare_examples[key].present?
+                                                  Settings.compare_examples[key].map do |list|
+                                                    locations = expand_location_values(list, key)
+                                                    next if locations.blank?
+
+                                                    [locations.map(&:first).join(" + "), locations.map(&:last).join(":")]
+                                                  end.compact
+                                                else
+                                                  DEFAULT_COMPARE_EXAMPLES[key]
+                                                end
+                              end
+                            end
+    end
+
+    def expand_location_values(slugs, places_collection)
+      return unless slugs.present?
+
+      slugs.map do |slug|
+        location = PlaceDecorator.find_by_slug(slug, places_collection:)
+        next if location.blank?
+
+        [location.name, location.slug]
+      end.compact
     end
   end
 end
